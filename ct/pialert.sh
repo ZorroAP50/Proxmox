cf62d8063ba3a72f4dcd466d3abdf5b38944eba4
ZorroAP50 <arturprus26@gmail.com>
2026-04-16 08:30:48 +0200

    Update source URL for build functions

--- ct/pialert.sh
+++ ct/pialert.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/ZorroAP50/Proxmox/main/misc/build.func)
-# Copyright (c) 2021-2024 tteck
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Author: tteck (tteckster)
 # License: MIT
 # https://github.com/tteck/Proxmox/raw/main/LICENSE
