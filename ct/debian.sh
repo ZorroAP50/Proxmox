0b85c93032513704252b81972f0846d7e35af040
ZorroAP50 <arturprus26@gmail.com>
2026-05-20 12:22:57 +0200

    Update Debian version from 12 to 13

--- ct/debian.sh
+++ ct/debian.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/tteck/Proxmox/main/misc/build.func)
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
@@ -23,7 +23,7 @@ var_disk="2"
 var_cpu="1"
 var_ram="512"
 var_os="debian"
-var_version="12"
+var_version="13"
 variables
 color
 catch_errors
